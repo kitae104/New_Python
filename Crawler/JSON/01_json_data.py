8452493bb6c63a4923ecbf5c3999057f7b4bbfb1
kitae <aqua0405@gmail.com>
2019-08-01 09:24:18 +0900

    json data

---
+++ Crawler/JSON/01_json_data.py
@@ -0,0 +1,15 @@
+# 1개의 값을 갖는 json
+{"name":"hong"}
+
+# 2개의 값을 갖는 json
+{"name":"hong", "age":32}
+
+# 리스트에 하나의 데이터 [1] 과 같은 형태
+[{"name":"hong", "age":32}]
+
+# 테이블 형태의 데이터를 json으로 표현
+[
+    {"name":"hong", "age":"32", "math":"90"},
+    {"name":"an", "age":"22", "math":"85"}
+]
+
