7a512791e121ecf4c603433f1dbc9867a03b65b5
kitae <aqua0405@gmail.com>
2019-08-03 16:26:09 +0900

    bs4 태그 프로퍼티 다루기

--- Crawler/bs4/02_bs4_tag_property.py
+++ Crawler/bs4/02_bs4_tag_property.py
@@ -19,4 +19,20 @@ html_str = """
 
 # 해당 문자열을 html.parser 를 통해서 읽어와 객체를 생성
 bs_obj = bs4.BeautifulSoup(html_str, "html.parser")
-print(bs_obj)
+# print(bs_obj)
+
+ul_greet = bs_obj.find("ul")    # 첫번째 ul 추출
+# print(ul_reply)
+
+# 태그와 속성을 함께 사용해서 정보 추출
+ul_reply = bs_obj.find("ul", {"class":"reply"}) # class 속성이 reply인 경우 추출
+print(ul_reply)
+
+lis = ul_reply.findAll("li")
+print(lis)
+
+for li in lis:
+    print(li)
+
+for li in lis:
+    print(li.text)
