38324421e62794724158532e132c886124de788d
kitae <aqua0405@gmail.com>
2019-11-13 01:08:18 +0900

    시퀀스 데이터 타입

---
+++ DataStructure/Sequence/sequence_type.py
@@ -0,0 +1,20 @@
+# 내장 시퀀스 타입
+l = []
+print(type(l))
+# <class 'list'>
+
+s = ""
+print(type(s))
+# <class 'str'>
+
+t = ()
+print(type(t))
+# <class 'tuple'>
+
+ba = bytearray(b"")
+print(type(ba))
+# <class 'bytearray'>
+
+b = bytes([])
+print(type(b))
+# <class 'bytes'>
