e070b8f61e157cf9ebe3381b2fa51e57921aa04e
kitae104 <aqua0405@gmail.com>
2019-12-31 13:29:11 +0900

    3장

--- DeepLearningFromScratch/ch03/neural_lib.py
+++ DeepLearningFromScratch/ch03/neural_lib.py
@@ -20,3 +20,15 @@ def sigmoid(x):
 # ReLU 함수
 def relu(x):
     return np.maximum(0, x)
+
+# identity_function 함수(항등 함수)
+def identity_function(x):
+    return x
+
+# softmax 함수
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
