1f626b3d2e413d3fb44cbbf75f2733eab47a7505
kitae104 <aqua0405@gmail.com>
2020-01-14 12:02:17 +0900

    선형 회귀

---
+++ DeepLearningZeroToAll/ch02/linear_regression.py
@@ -0,0 +1,32 @@
+# 선형 회귀 구현하기
+import tensorflow as tf
+
+# x_train = [1, 2, 3]
+# y_train = [1, 2, 3]
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+
+W = tf.Variable(tf.random_normal([1]), name="weight")
+b = tf.Variable(tf.random_normal([1]), name="bias")
+
+hypothesis = X * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)    # cost를 최소화 함
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())    # 변수 사용시 초기화
+
+for step in range(2001):
+    cost_val, W_val, b_val, _ = sess.run([cost, W, b, train], feed_dict={X:[1,2,3,4,5], Y:[2.1,3.1, 4.1,5.1, 6.1]})
+
+    if step % 20 == 0:
+        print(step, cost_val, W_val, b_val)
+
+print(sess.run(hypothesis, feed_dict={X:[5]}))
+print(sess.run(hypothesis, feed_dict={X:[2.5]}))
+print(sess.run(hypothesis, feed_dict={X:[1.5, 3.5]}))
