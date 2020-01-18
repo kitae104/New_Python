550633e91e23713257538add5818c41eef9f0edc
kitae <aqua0405@gmail.com>
2020-01-19 08:56:28 +0900

    1차원 2차원 배열에서 슬라이스

--- DeepLearningZeroToAll/test/slicing.py
+++ DeepLearningZeroToAll/test/slicing.py
@@ -8,3 +8,27 @@ print(nums[:])
 print(nums[:-1])
 nums[2:4] = [8, 9]
 print(nums)
+
+# numpy의 array 연습
+import numpy as np
+
+# 1차원 배열
+a = np.array([1,2,3,4,5])
+print(a)
+print(a[1:3])
+print(a[-1])
+a[0:2] = 9
+print(a)
+
+# 2차원 배열
+b = np.array([[1,2,3,4],
+              [5,6,7,8],
+              [9,10,11,12]
+            ])
+print(b)
+
+print(b[:, 1])      # 전체 행에서 2번째 열 출력
+print(b[-1])        # 마지막 행 출력
+print(b[-1, :])     # 마지막 행 출력
+print(b[-1, ...])   # 마지막 행 출력
+print(b[0:2, :])    # 1, 2행 출력
