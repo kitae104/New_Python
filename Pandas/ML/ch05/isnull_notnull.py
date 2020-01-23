e1194f191963d58761cb94f5b3e7469e00011cdd
kitae <aqua0405@gmail.com>
2020-01-24 01:50:26 +0900

    없는 데이터에 대한 처리

--- Pandas/ML/ch05/isnull_notnull.py
+++ Pandas/ML/ch05/isnull_notnull.py
@@ -42,6 +42,20 @@ print(df['age'].head(10))
 # age 열의 NaN값을 다른 나이 데이터의 평균으로 변경하기
 mean_age = df['age'].mean(axis=0)   # age 열의 평균 계산 (NaN 값 제외)
 print(mean_age)
-df['age'].fillna(mean_age, inplace=True)
+df['age'].fillna(mean_age, inplace=True)    # nan 부분을 새로운 값으로 대체
 
 print(df['age'].head(10))
+
+# embark_town 열의 829행의 NaN 데이터 출력
+print(df['embark_town'][825:830])
+
+# embark_town 열의 NaN값을 승선도시 중에서 가장 많이 출현한 값으로 치환하기
+most_freq = df['embark_town'].value_counts(dropna=True).idxmax()
+print(most_freq)
+df['embark_town'].fillna(most_freq, inplace=True)   # nan 부분을 새로운 값으로 대체
+
+print(df['embark_town'][825:830])
+
+# embark_town 열의 NaN값을 바로 앞에 있는 828행의 값으로 변경하기
+df['embark_town'].fillna(method='ffill', inplace=True)
+print(df['embark_town'][825:830])
