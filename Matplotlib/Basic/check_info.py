e4bf087008d105396daf305d23e74a9248006a2c
kitae104 <aqua0405@>
2020-02-08 23:32:56 +0900

    여러 정보 확인

--- Matplotlib/Basic/check_info.py
+++ Matplotlib/Basic/check_info.py
@@ -20,3 +20,14 @@ print(font_list[:10])
 
 # 스타일 리스트 출력
 print(plt.style.available)
+
+# 컬러 정보를 담을 빈 딕셔너리 생성
+colors = {}
+
+# 컬러 이름과 헥사코드 확인하여 딕셔서리에 입력
+for name, hex in mpl.colors.cnames.items():
+    colors[name] = hex
+
+#print(colors)
+for key, value in colors.items():
+    print(key, ":", value)
