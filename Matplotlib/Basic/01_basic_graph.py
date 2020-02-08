b78456dbf64f3f8d4299bb29d12c41d3e22c871b
kitae104 <aqua0405@>
2020-02-08 21:27:47 +0900

    기본 그래프

--- Matplotlib/Basic/01_basic_graph.py
+++ Matplotlib/Basic/01_basic_graph.py
@@ -1,6 +1,31 @@
 # 기본 그래프
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4])     # 기본적으로 파란색 실선 그래프
-plt.ylabel('some number')   # y측 라벨
+# matplotlib 한글 폰트 오류 문제 해결
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/malgun.ttf"   #폰트파일의 위치
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+
+# 그림 크기 지정
+plt.figure(figsize=(6, 5))     # 가로 6인치, 세로 5인치
+
+# 축에 대한 정보
+plt.axis([0, 6, 0, 10])     # x축은 0~6, y축은 0~10
+
+# 데이터와 모양 선택
+plt.plot([1,2,3,4], [2,4, 6, 8])     # 그래프, 파란색 선
+# plt.plot([1,2,3,4], [2,4, 6, 8], '*')     # 그래프, 파란색 별표
+
+# 타이틀 설정
+plt.title('타이틀')
+
+# x, y 축 라벨
+plt.xlabel('x label')   # y측 라벨
+plt.ylabel('y label')   # y측 라벨
+
+#범례 표시
+plt.legend(labels=['범례1'], loc='best')
+
+# 그래프 보기
 plt.show()
