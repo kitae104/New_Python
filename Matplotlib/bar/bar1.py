3d2337113e0c5fe4100d13f124029d98d9ab55ce
kitae104 <aqua0405@>
2020-02-11 00:01:43 +0900

    막대그래프1

--- Matplotlib/bar/bar1.py
+++ Matplotlib/bar/bar1.py
@@ -7,3 +7,7 @@ from matplotlib import font_manager, rc
 font_path = "../datas/시도별 전출입 인구수.xlsx" # 폰트 경로
 font_name = font_manager.FontProperties(fname=font_path).getName()
 rc('font', family=font_name)
+
+# IPyhton 디스플레이 설정 변경
+pd.set_option('display.max_columns', 10)    # 보여지는 열의 개수(안보이는 부분은 ... 으로)
+pd.set_option('display.max_colwidth', 10)    # 출력할 열의 너비
