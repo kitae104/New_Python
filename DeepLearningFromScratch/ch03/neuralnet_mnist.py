bf25c5ab08a3b196a8571e14f0ae7cc489bda9f5
kitae104 <aqua0405@>
2020-02-15 00:04:17 +0900

    mnist 신경망 처리

--- DeepLearningFromScratch/ch03/neuralnet_mnist.py
+++ DeepLearningFromScratch/ch03/neuralnet_mnist.py
@@ -5,6 +5,7 @@ import pickle
 from DeepLearningFromScratch.dataset.mnist import load_mnist
 from DeepLearningFromScratch.common.functions import sigmoid, softmax
 
+# normalize - True의 경우 0~255의 범위가 0.0~1.0 번위로 변환됨
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=False)
     return x_test, t_test
@@ -38,7 +39,7 @@ if __name__ == "__main__":
     for i in range(len(x)):
         y = predict(network, x[i])
         p = np.argmax(y)    # 확률이 가장 높은 원소의 인덱스를 얻는다.
-        if p == t[i]:
+        if p == t[i]:       # 예측과 정답이 맞는지 확인한다.
             accuracy_cnt += 1
 
     print("정확도 :" + str(float(accuracy_cnt) / len(x)))
