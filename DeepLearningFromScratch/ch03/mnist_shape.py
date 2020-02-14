47d6de0f613127215c1612b188e021bdc2da145a
kitae104 <aqua0405@>
2020-02-14 23:32:25 +0900

    MNIST 형상 출력

--- DeepLearningFromScratch/ch03/mnist_shape.py
+++ DeepLearningFromScratch/ch03/mnist_shape.py
@@ -1,7 +1,7 @@
 # MNIST 정보 출력
 import sys, os
 sys.path.append(os.pardir)      # 부모디렉토리로 접근
-from dataset.mnist import load_mnist
+from DeepLearningFromScratch.dataset.mnist import load_mnist
 
 # 데이터 로딩하기
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
