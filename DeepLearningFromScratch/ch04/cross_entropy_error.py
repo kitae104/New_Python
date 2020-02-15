5aeff801609302d50a71662be4e50187f035d65e
kitae104 <aqua0405@>
2020-02-15 20:37:10 +0900

    교차 엔트로피 오차

--- DeepLearningFromScratch/ch04/cross_entropy_error.py
+++ DeepLearningFromScratch/ch04/cross_entropy_error.py
@@ -5,6 +5,15 @@ def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
 
+def cross_entropy_error2(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size   # 평균 교차 엔트로피 
+
+
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 t = [0,0,1,0,0,0,0,0,0,0]
 print(cross_entropy_error(np.array(y), np.array(t)))    # 0에 가까울 수록 정답
