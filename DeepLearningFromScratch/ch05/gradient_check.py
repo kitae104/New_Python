251bb366479fc64961437f266146372a7b481197
kitae104 <aqua0405@>
2020-02-17 23:54:27 +0900

    기울기 비교

--- DeepLearningFromScratch/ch05/gradient_check.py
+++ DeepLearningFromScratch/ch05/gradient_check.py
@@ -1,3 +1,4 @@
+# 수치 미분과 오차역전파 비교
 import sys, os
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
@@ -11,6 +12,7 @@ network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 x_batch = x_train[:3]
 t_batch = t_train[:3]
+print(t_batch)
 
 grad_numerical = network.numerical_gradient(x_batch, t_batch)
 grad_backprop = network.gradient(x_batch, t_batch)
