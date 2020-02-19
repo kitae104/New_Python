80015f3667be8d1aed8c00bcf9b6f5263208494c
kitae104 <aqua0405@gmail.com>
2020-02-19 10:30:51 +0900

    Layer 클래스 구현

--- DeepLearningFromScratch/ch01_2/layer_classes.py
+++ DeepLearningFromScratch/ch01_2/layer_classes.py
@@ -1,6 +1,7 @@
 # Layer 클래스 구현
 import numpy as np
-from DeepLearningFromScratch.common.functions import sigmoid
+from DeepLearningFromScratch.common.functions import sigmoid, softmax, cross_entropy_error
+
 
 class MatMul:
     def __init__(self, W):
@@ -60,5 +61,32 @@ class Affine:
 
         self.grads[0][...] = dW
         self.grads[1][...] = db
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)     # 소프트 맥스 사용
+
+        # 정답 레이블이 원핫 벡터일 경우 정답의 인덱스로 변환
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)  # 교차 엔트피 오류 사용 
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
 
         return dx
