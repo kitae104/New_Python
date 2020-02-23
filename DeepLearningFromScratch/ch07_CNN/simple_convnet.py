f6facec0708da8a50e1f5f497b3ff42fd40fba05
kitae104 <aqua0405@>
2020-02-23 14:38:47 +0900

    공통으로 사용할 트레이너 클래스

--- DeepLearningFromScratch/ch07_CNN/simple_convnet.py
+++ DeepLearningFromScratch/ch07_CNN/simple_convnet.py
@@ -1,5 +1,9 @@
+import pickle
+
 import numpy as np
 from collections import OrderedDict
+
+from DeepLearningFromScratch.common.gradient import numerical_gradient
 from DeepLearningFromScratch.common.layers import *
 
 class SimpleConvNet:
@@ -56,7 +60,7 @@ class SimpleConvNet:
         self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], conv_param['stride'], conv_param['pad'])
         self.layers['Relu1'] = Relu()
         self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
-        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b'])
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
         self.layers['Relu2'] = Relu()
         self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
         self.last_layer = SoftmaxWithLoss()
@@ -112,3 +116,101 @@ class SimpleConvNet:
         grads['b3'] = self.layers['Affine2'].db
 
         return grads    # 기울기 정보 반환
+
+    ######################################
+    # 정확도 출력
+    ######################################
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1:                 # 정답의 차수가 1이 아니면
+            t = np.argmax(t, axis=1)    # 각 행에서 제일 큰 값을 추출
+
+        acc = 0.0       # 정확도 초기화
+
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]   # 배치 데이터
+            tt = t[i*batch_size:(i+1)*batch_size]   # 배치 정답
+            y = self.predict(tx)                    # 추축값 계산
+            y = np.argmax(y, axis=1)                # 행에서 추축값 중에 제일 큰값 선택
+            acc += np.sum(y == tt)
+
+        return acc / x.shape[0]                       # 평균을 구해서 반환
+
+    ######################################
+    # 파리미터 pkl 파일로 저장
+    ######################################
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    ######################################
+    # pkl 파일로부터 파라미터 읽어오기
+    ######################################
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i + 1)]
+            self.layers[key].b = self.params['b' + str(i + 1)]
+
+    def numerical_gradient(self, x, t):
+        """기울기를 구한다（수치미분）.
+
+        Parameters
+        ----------
+        x : 입력 데이터
+        t : 정답 레이블
+
+        Returns
+        -------
+        각 층의 기울기를 담은 사전(dictionary) 변수
+            grads['W1']、grads['W2']、... 각 층의 가중치
+            grads['b1']、grads['b2']、... 각 층의 편향
+        """
+        loss_w = lambda w: self.loss(x, t)
+
+        grads = {}
+        for idx in (1, 2, 3):
+            grads['W' + str(idx)] = numerical_gradient(loss_w, self.params['W' + str(idx)])
+            grads['b' + str(idx)] = numerical_gradient(loss_w, self.params['b' + str(idx)])
+
+        return grads
+
+    def gradient(self, x, t):
+        """기울기를 구한다(오차역전파법).
+
+        Parameters
+        ----------
+        x : 입력 데이터
+        t : 정답 레이블
+
+        Returns
+        -------
+        각 층의 기울기를 담은 사전(dictionary) 변수
+            grads['W1']、grads['W2']、... 각 층의 가중치
+            grads['b1']、grads['b2']、... 각 층의 편향
+        """
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
