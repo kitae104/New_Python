6b193c202c99bf5256883c964129d7b7962ddd79
kitae104 <aqua0405@gmail.com>
2020-05-06 01:29:15 +0900

    예측 모델

--- RNN/예측모델/test.py
+++ RNN/예측모델/test.py
@@ -1,15 +1,38 @@
-import numpy as np
 import tensorflow as tf
-# 데이터 생성
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([1,2,3,4,5,6,7,8,9,10])
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-model = Sequential()
+import numpy as np
+import os
 
-model.add(Dense(1, input_dim=1, activation='relu'))
-model.compile(loss='mean_squared_error',optimizer='adam',metrics=['accuracy'])
-model.fit(x, y, epochs= 500, batch_size=1)
-loss, acc = model.evaluate(x, y, batch_size=1)
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
+x_train = np.array([1,2,3,4,5,6,7,8,9,10])
+y_train = np.array([1,2,3,4,5,6,7,8,9,10])
+x_test = np.array([101,102,103,104,105,106,107,108,109,110])
+y_test = np.array([101,102,103,104,105,106,107,108,109,110])
+
+model = Sequential()
+model.add(Dense(5, input_dim =1 , activation='relu'))
+model.add(Dense(3))
+model.add(Dense(1, activation='relu'))
+model.summary()
+
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_data=(x_test, y_test))
+loss, acc = model.evaluate(x_test, y_test, batch_size =1)
+
 print("loss : ", loss)
 print("acc : ", acc)
+y_predict = model.predict(x_test)
+print("결과물 : \n", y_predict)
+
+# RMSE 구하기
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE : ", RMSE(y_test, y_predict))
+
+# R2 구하기
+from sklearn.metrics import r2_score
+r2_y_predict = r2_score(y_test, y_predict)
+print("R2 : ", r2_y_predict)
