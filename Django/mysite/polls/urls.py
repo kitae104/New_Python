82a48264cd51a927b15bed3c848187e6a289c10f
kitae104 <aqua0405@gmail.com>
2020-06-29 01:00:01 +0900

    Django 수정

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'polls'
 
 # 해당 앱에 대한 URL 패턴을 생성하고 연결한다. 
+#  <question_id> 에서 <pk> 로 변경
 urlpatterns = [
   # ex: /polls/
-  path('', views.index, name='index'),
+  path('', views.IndexView.as_view(), name='index'),
   # ex: /polls/5/  이때 question_id은 뷰에서 넘어온 데이터 임 
-  path('<int:question_id>/', views.detail, name='detail'),
+  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
   # ex: /polls/5/results/
-  path('<int:question_id>/results/', views.results, name='results'),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
   # ex: /polls/5/vote/
   path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,34 +1,62 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from polls.models import Question, Choice
 from django.template import loader
-from django.shortcuts import render
 from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 
 # 새로운 뷰는 여기에 작성 .
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    # try:
-    #   question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #   raise Http404("Question does not exist!")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+# def detail(request, question_id):
+#     # try:
+#     #   question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #   raise Http404("Question does not exist!")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question':question})
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # GET과 POST 방식으로 각각 처리할 때 
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        question = get_object_or_404(Question, pk=question_id)
+
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
