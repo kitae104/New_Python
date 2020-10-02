454caa9e2746aa03a0756f3f53ad70dfa2c65d38
kitae104 <aqua0405@gmail.com>
2020-10-02 16:21:57 +0900

    MNIST(기본 프레임)

---
+++ DL/CNN_RNN_GAN/mnist_basic_frame.py
@@ -0,0 +1,75 @@
+# MNIST 손글씨 인식(기본 프레임)
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import matplotlib.pyplot as plt
+
+# seed 값 설정
+seed = 0
+np.random.seed(seed)
+tf.random.set_seed(3)
+
+# MNIST데이터셋 불러오기
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+# 차원 변환 과정 (reshape(샘플수, 1차원 속성 수) -> reshape(6000, 784))
+X_train = X_train.reshape(X_train.shape[0], 784).astype('float64') / 255
+X_test = X_test.reshape(X_test.shape[0], 784).astype('float64') / 255
+
+# 원-핫 인코딩 형태로 변환하기
+Y_train = to_categorical(Y_train, 10)
+Y_test = to_categorical(Y_test, 10)
+
+# 모델 설정하기
+model = Sequential()
+model.add(Dense(512, input_dim=784, activation='relu'))
+model.add(Dense(10, activation='softmax'))              # 확률 추가
+
+# 모델 컴파일
+model.compile(loss='categorical_crossentropy',      # 여러개중에 1개를 선택하는 경우
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# 모델 최적화 설정
+MODEL_DIR = './model_mnist/'          # 폴더 설정
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+# 모델 저장 조건 설정
+modelpath="./model_mnist/{epoch:02d}-{val_loss:.4f}.hdf5"
+checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
+
+# 학습 자동 중단 설정 - 모니터링 대상과 기다릴 횟수 설정
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
+
+# 모델 실행(verbose = 0/1)
+hist = model.fit(X_train, Y_train, epochs=100, batch_size=200,
+                 validation_data=(X_test, Y_test), verbose=0, callbacks=[checkpointer, early_stopping_callback])
+
+# 테스트 정확도 출력
+print("\n Test Accuracy: %.4f" % (model.evaluate(X_test, Y_test)[1]))
+
+# 그래프 그리기
+# 테스트 셋의 오차
+y_vloss = hist.history['val_loss']
+
+# 학습셋의 오차
+y_loss = hist.history['loss']
+
+# 그래프로 표현
+x_len = np.arange(len(y_loss))
+plt.plot(x_len, y_vloss, marker='.', c="red", label='Testset_loss')
+plt.plot(x_len, y_loss, marker='.', c="blue", label='Trainset_loss')
+
+# 그래프에 그리드를 주고 레이블을 표시
+plt.legend(loc='upper right')
+# plt.axis([0, 20, 0, 0.35])
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
