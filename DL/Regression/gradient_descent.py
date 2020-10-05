d2ca8102e2c3e5c08515ed2bc46034cb0a1204dd
kitae104 <aqua0405@gmail.com>
2020-10-05 14:22:07 +0900

    gradient descent

--- DL/Regression/gradient_descent.py
+++ DL/Regression/gradient_descent.py
@@ -6,3 +6,14 @@ import matplotlib.pyplot as plt
 
 # 공부시간 X와 성적 Y의 리스트 만들기
 data = [[2, 81], [4, 93], [6, 91], [8, 97]]
+
+import pandas as pd
+
+data = {
+    'year': [2016, 2017, 2018],
+    'GDP rate': [2.8, 3.1, 3.0],
+    'GDP': ['1.637M', '1.73M', '1.83M']
+}
+
+df = pd.DataFrame(data)
+print(df)
