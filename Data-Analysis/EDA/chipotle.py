3884b8939f27f2342c4c52775c28794680b8e300
kitae104 <aqua0405@gmail.com>
2020-11-07 00:56:12 +0900

    수정

--- Data-Analysis/EDA/chipotle.py
+++ Data-Analysis/EDA/chipotle.py
@@ -3,7 +3,6 @@ import pandas as pd
 file_path = '../data/chipotle.tsv'
 chipo = pd.read_csv(file_path, sep='\t')
 
-<<<<<<< HEAD
 item_count = chipo['item_name'].value_counts()
 
 for idx, (val, cnt) in enumerate(item_count.iteritems()):
