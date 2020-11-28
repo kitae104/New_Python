e05b52ade65f4e3015689e64fef31fcdb3861a04
kitae104 <aqua0405@gmail.com>
2020-11-29 01:25:48 +0900

    테스트 1

--- Test/Basic/test1.py
+++ Test/Basic/test1.py
@@ -1,10 +1,26 @@
-month = '3'
-if month == 5:
-  print('5월 예약 가능')
-elif month == 6:
-  print('6월 예약 가능')
-elif month == 7:
-  print('7월 예약 가능')
-else:
-  print('5월~7월만 예약 가능')
+import os
+import sys
 
+for i in range(1, 11):
+
+    if i == 5:
+        print(exit)
+        #break
+        os._exit(0)
+        #exit(0)
+        #quit(0)
+        #sys.exit(0)
+
+    print(i)
+
+
+for i in range(10):
+
+    # If the value of i becomes
+    # 5 then the program is forced
+    # to exit
+    if i == 5:
+        # prints the exit message
+        print(exit)
+        exit()
+    print(i)
