2fd6cb7a9f28f50a7421d8847d816531897d22e7
DESKTOP-28JJ3KT\김기태 <aqua0405@gmail.com>
2020-12-28 16:40:42 +0900

    CNN 모델

--- Chatbot/ch06_딥러닝_모델/cnn_model.py
+++ Chatbot/ch06_딥러닝_모델/cnn_model.py
@@ -7,3 +7,75 @@ from tensorflow.keras import preprocessing
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Embedding, Dense, Dropout, Conv1D, GlobalMaxPool1D, concatenate
 
+# 데이터 읽어오기
+train_file = "./chatbot_data.csv"
+data = pd.read_csv(train_file, delimiter=',')
+features = data['Q'].tolist()
+labels = data['label'].tolist()
+
+# 단어 인덱스 시퀀스 벡터
+corpus = [preprocessing.text.text_to_word_sequence(text) for text in features]
+print(corpus[0])    # ['12시', '땡']
+tokenizer = preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(corpus)
+sequences = tokenizer.texts_to_sequences(corpus)  # 모든 단어에 번호를 붙임
+print(sequences[0]) # [4646, 4647]
+
+word_index = tokenizer.word_index
+#print(word_index)
+MAX_SEQ_LEN = 15        # 단어 시퀀스 벡터 크기
+padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post') # 빈부분을 0으로 채우기
+print(padded_seqs[0])   # [4646 4647    0    0    0    0    0    0    0    0    0    0    0    0    0]
+
+# 학습용, 검증용, 테스트용 데이터셋 생성
+# 학습셋:검증셋:테스트셋 = 7:2:1
+ds = tf.data.Dataset.from_tensor_slices((padded_seqs, labels))
+ds = ds.shuffle(len(features))  # 11823
+train_size = int(len(padded_seqs) * 0.7)    # 8276
+val_size = int(len(padded_seqs) * 0.2)      # 2364
+test_size = int(len(padded_seqs) * 0.1)     # 1182
+
+train_ds = ds.take(train_size).batch(20)
+val_ds = ds.skip(train_size).take(val_size).batch(20)
+test_ds = ds.skip(train_size + val_size).take(test_size).batch(20)
+
+# 하이퍼파라미터 설정
+dropout_prob = 0.5      # 50%
+EMB_SIZE = 128          # 밀집 벡터 크기
+EPOCH = 5
+VOCAB_SIZE = len(word_index) + 1  # 전체 단어 수 - 13399
+
+# CNN 모델 정의
+input_layer = Input(shape=(MAX_SEQ_LEN,))   # 입력 계층
+embedding_layer = Embedding(VOCAB_SIZE, EMB_SIZE, input_length=MAX_SEQ_LEN)(input_layer)
+dropout_emb = Dropout(rate=dropout_prob)(embedding_layer)
+
+# 임베딩 벡터에서 특징 추출하는 영역
+conv1 = Conv1D(filters=128, kernel_size=3, padding='valid', activation=tf.nn.relu)(dropout_emb)
+pool1 = GlobalMaxPool1D()(conv1)
+conv2 = Conv1D(filters=128, kernel_size=4, padding='valid', activation=tf.nn.relu)(dropout_emb)
+pool2 = GlobalMaxPool1D()(conv2)
+conv3 = Conv1D(filters=128, kernel_size=5, padding='valid', activation=tf.nn.relu)(dropout_emb)
+pool3 = GlobalMaxPool1D()(conv3)
+
+# 3, 4, 5- gram 이후 합치기
+concat = concatenate([pool1, pool2, pool3])
+hidden = Dense(128, activation=tf.nn.relu)(concat)
+dropout_hidden = Dropout(rate=dropout_prob)(hidden)
+logits = Dense(3, name='logits')(dropout_hidden)            # 3가지 클래스로 감정 분류
+predictions = Dense(3, activation=tf.nn.softmax)(logits)    # 감정 클래스별 확률로 계산
+
+# 모델 생성
+model = Model(inputs=input_layer, outputs=predictions)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# 모델 학습
+model.fit(train_ds, validation_data=val_ds, epochs=EPOCH, verbose=1)
+
+# 모델 평가(테스트 데이터셋 이용)
+loss, accuracy = model.evaluate(test_ds, verbose=1)
+print('Accuracy: %f' % (accuracy * 100))
+print('loss: %f' % (loss))
+
+# 모델 저장
+model.save('cnn_model.h5')
