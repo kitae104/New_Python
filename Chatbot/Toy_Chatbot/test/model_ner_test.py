1ae24c6894b03bf2b8931ca22ef65423144c3600
kitae104 <aqua0405@gmail.com>
2021-01-11 00:19:55 +0900

    NerModel 객체 사용 테스트

---
+++ Chatbot/Toy_Chatbot/test/model_ner_test.py
@@ -0,0 +1,14 @@
+# NerModel 객체 사용 테스트
+from utils.Preprocess import Preprocess
+from models.ner.NerModel import NerModel
+
+p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin', userdic='../utils/user_dic.tsv')
+
+ner = NerModel(model_name='../models/ner/ner_model.h5', proprocess=p)
+
+query = '오늘 오전 13시 2분에 탕수육 주문 하고 싶어요'
+
+predicts = ner.predic(query)
+tags = ner.predict_tags(query)
+print(predicts)
+print(tags)
